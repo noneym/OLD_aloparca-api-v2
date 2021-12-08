74fe389e4a5a92d04480183745ff4b8d61fc5b39
Efe Mert Demir <emdemir.emd@gmail.com>
2021-12-08 13:41:39 +0000

    Config: Move to Europe/Istanbul timezone
    The "local" timezone is not always available (didn't seem to work in
    Docker), so let's just specify the timezone that we know we will use.

--- api/app/Config/App.php
+++ api/app/Config/App.php
@@ -108,7 +108,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $appTimezone = 'local';
+    public $appTimezone = 'Europe/Istanbul';
 
     /**
      * --------------------------------------------------------------------------
